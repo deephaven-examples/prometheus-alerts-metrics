2bfedfd64daf5c74f49b3de5f387299513bc26a7
Jake Mulford <jakemulf@gmail.com>
2022-01-04 15:20:43 -0500

    added flask app build to workflow check

--- prometheusDhAlertMetricsStart.sh
+++ prometheusDhAlertMetricsStart.sh
@@ -1,3 +1,3 @@
 docker build --tag prometheus-deephaven/grpc-api .
-docker build --tag local-flask/prometheus-webhook-alerts flask-app
+docker build --tag flask/prometheus-webhook-alerts flask-app
 docker-compose up
